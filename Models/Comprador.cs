c98bc6b6cf9164edfa32b2b7868c4c332af9a9c3
Gustavo Mayorga <redvioletoficial@gmail.com>
2024-02-20 15:29:12 -0600

    Agregar archivos de proyecto.

---
+++ Models/Comprador.cs
@@ -0,0 +1,27 @@
+﻿using MessagePack;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Practica_14_02_2024.Models
+{
+    [Table("Comprador")] //Definir que este va a ser un model llamada Comprador
+    public class Comprador
+    {
+        [System.ComponentModel.DataAnnotations.Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdComprador { get; set; }
+        public string Nombre { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Correo { get; set; }
+
+        public Boolean Activo { get; set; }
+
+        public String URLpath { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime FechaRegistro {get; set;}
+
+        public virtual Comprador comprador { get; set; }
+    }
+}
---
+++ Models/OrdenCompra.cs
@@ -0,0 +1,26 @@
+﻿namespace Practica_14_02_2024.Models
+{
+    public class OrdenCompra
+    {
+        idOrdenCompra int auto_increment primary key,
+        consecutivo varchar(200),
+proyecto varchar(200),
+status int default 1,
+no_consecutivo int,
+referencia varchar(200),
+fecha date,
+empresa varchar(200),
+vendedor varchar(200),
+direccion varchar(200),
+subtotal real,
+iva real,
+total real,
+moneda varchar(20),
+condicionPago varchar(200),
+entrega varchar(200),
+tiempoEntrega varchar(200),
+activo bool default true,
+idCompradorFK int,
+foreign key(idCompradorFK) references Comprador(idcomprador) on delete cascade
+    }
+}
